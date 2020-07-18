2ec8ea10f656bce2d0a18616b39f0fa3649f96f3
Charan Teja <charaneie22@gmail.com>
2020-07-18 23:51:14 +0530

    add signup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Components/login';
-import { BrowserRouter , Route, Link } from 'react-router-dom';
-import Register from './Components/register';
-import Home from './Components/Home';
-
+import React from "react";
+import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./Components/login";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Register from "./Components/register";
+import Movies from "./Components/Movies";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Route exact path="/" exact component={Home}/>
-      {/* <Route exact path="/" exact component={Content}/> */}
-      <Route exact path='/register' exact component={Register}/>
-      <Route exact path='/login' exact component={Login}/>
-
-
+        {/* <Route exact path="/" exact component={Home} /> */}
+        {/* <Route exact path="/" exact component={Content}/> */}
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route path="/movies" component={Movies} />
       </BrowserRouter>
     </div>
   );
--- src/Components/register.js
+++ src/Components/register.js
@@ -1,11 +1,59 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Register extends Component {
-  render() {
-    return (
-      <div>This is my signup page.</div>
-    );
-  }
-}
+const SignUp = () => {
+  const handleSubmit = (data) => {
+    console.log(data, "data");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
 
-export default Register;
+      <div className="form-group">
+        <label>Name</label>
+        <input type="text" className="form-control" placeholder="First name" />
+      </div>
+
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Phone Number</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Phone number"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Profession</label>
+        <select class="custom-select">
+          <option selected>Select a Job</option>
+          <option value="1">JAVA dev</option>
+          <option value="2">REACT dev</option>
+        </select>
+      </div>
+
+      <button type="submit" className="btn btn-primary btn-block">
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default SignUp;
